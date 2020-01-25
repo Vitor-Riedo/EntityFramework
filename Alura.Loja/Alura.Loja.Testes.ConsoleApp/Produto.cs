7b19c0798ff851a97086e8824a5153739e5a8b76
Vitor Riedo <vitorriedo@hotmail.com>
2020-01-25 12:25:58 -0200

    Entity Framework Core: Relacionamentos 1:1, 1:N, N:N - Aulas 4,5,6 e 7

--- Alura.Loja/Alura.Loja.Testes.ConsoleApp/Produto.cs
+++ Alura.Loja/Alura.Loja.Testes.ConsoleApp/Produto.cs
@@ -1,16 +1,20 @@
-﻿namespace Alura.Loja.Testes.ConsoleApp
+﻿using System.Collections.Generic;
+
+namespace Alura.Loja.Testes.ConsoleApp
 {
-    internal class Produto
+    public class Produto
     {
         public int Id { get; internal set; }
         public string Nome { get; internal set; }
         public string Categoria { get; internal set; }
-        public double Preco { get; internal set; }
+        public double PrecoUnitario { get; internal set; }
+        public string Unidade { get; internal set; }
+        public IList<PromocaoProduto> Promocoes{ get; internal set; }
 
         public override string ToString()
         {
             return  string.Format("Produto: {0}\n" +
-                                  "Preço:   {1}\n\n",this.Nome,this.Preco);
+                                  "Preço:   {1}\n\n",this.Nome,this.PrecoUnitario);
         }
     }
 }
--- Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Alura.Loja.Testes.ConsoleApp
 {
@@ -10,14 +6,75 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
-            using (var contexto = new ProdutoDAOEntity())
-            {
-                IList<Produto> listaProduto = contexto.Produtos().ToList();
+            //Exemplo 1:1
 
-                foreach (var p in listaProduto)
-                {
-                    Console.WriteLine(p.ToString());
-                }
+            var fulano = new Cliente();
+            fulano.Nome = "Riedo Teste";
+
+            fulano.EnderecoDeEntrega = new Endereco()
+            {
+                Numero = 12,
+                Logradouro = "Rua São",
+                Complemento = "Casa Grande",
+                Bairro = "Centro",
+                Cidade = "São Paulo"
+            };
+
+            using (var contexto = new LojaContex())
+            {
+                contexto.Clientes.Add(fulano);
+                contexto.SaveChanges();
+            }
+        }
+
+        static void Relacionamento_1_N()
+        {
+            //Exemplo 1:N
+
+            var paoFrances = new Produto();
+            paoFrances.Nome = "Pão Francês";
+            paoFrances.PrecoUnitario = 0.40;
+            paoFrances.Unidade = "Unidade";
+            paoFrances.Categoria = "Padaria";
+
+            var compra = new Compra();
+            compra.Quantidade = 6;
+            compra.Produto = paoFrances;
+            compra.Preco = paoFrances.PrecoUnitario * compra.Quantidade;
+
+            using (var contexto = new LojaContex())
+            {
+                contexto.Compras.Add(compra);
+
+                contexto.SaveChanges();
+            }
+        }
+
+        static void Relacionamento_N_N()
+        {
+            //Exemplo N:N
+
+            var p1 = new Produto() { Nome = "Suco de Laranja", Categoria = "Bebidas", PrecoUnitario = 8.79, Unidade = "Litros" };
+            var p2 = new Produto() { Nome = "Café", Categoria = "Bebidas", PrecoUnitario = 12.45, Unidade = "Gramas" };
+            var p3 = new Produto() { Nome = "Macarrão", Categoria = "Alimentos", PrecoUnitario = 4.23, Unidade = "Gramas" };
+
+            var promocaoDePascoa = new Promocao();
+            promocaoDePascoa.Descricao = "Páscoa Feliz";
+            promocaoDePascoa.DataInicio = DateTime.Now;
+            promocaoDePascoa.DataTermino = DateTime.Now.AddMonths(3);
+
+            promocaoDePascoa.IncluiProduto(p1);
+            promocaoDePascoa.IncluiProduto(p2);
+            promocaoDePascoa.IncluiProduto(p3);
+
+            using (var contexto = new LojaContex())
+            {
+                var promocao = contexto.Promocoes.Find(1);
+
+                contexto.Promocoes.Remove(promocao);
+
+                //contexto.Promocoes.Add(promocaoDePascoa);
+                contexto.SaveChanges();
             }
         }
     }
